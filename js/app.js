54b820374c6f5828ecfac1fc5192bd877a8f6907
Administrator <admin@example.com>
2021-09-20 16:19:17 +0200

    1. Agrega constantes y funciones iniciales

---
+++ js/app.js
@@ -0,0 +1,37 @@
+// Campos del formulario
+const mascotaInput = document.querySelector('#mascota');
+const propietarioInput = document.querySelector('#propietario');
+const telefonoInput = document.querySelector('#telefono');
+const fechaInput = document.querySelector('#fecha');
+const horaInput = document.querySelector('#hora');
+const sintomasInput = document.querySelector('#sintomas');
+
+//UI
+const formulario = document.querySelector('#nueva-cita');
+const contenedorCitas = document.querySelector('#citas');
+
+// Registar eventos
+eventListeners();
+function eventListeners() {
+    mascotaInput.addEventListener('input', datosCita);
+    propietarioInput.addEventListener('input', datosCita);
+    telefonoInput.addEventListener('input', datosCita);
+    fechaInput.addEventListener('input', datosCita);
+    horaInput.addEventListener('input', datosCita);
+    sintomasInput.addEventListener('input', datosCita);
+}
+
+// Objeto con información de la cita
+const citaObj = {
+    mascota: '',
+    propietario: '',
+    telefono: '',
+    fecha: '',
+    hora: '',
+    sintomas: ''
+}
+
+// Agrega datos al objeto de cita
+function datosCita(e) {
+    citaObj[e.target.name] = e.target.value;
+}
